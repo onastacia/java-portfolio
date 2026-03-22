7414185b1ec081cd6499fd68b07bb9902a9551f9
onastacia <rebbrevsd@gmail.com>
2026-03-22 21:09:37 +0300

    Add files via upload

--- expression-parser/Main.java
+++ expression-parser/Main.java
@@ -1,7 +1,5 @@
 package expression;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args){
         int value = Integer.parseInt(args[0]);
--- expression-parser/Variable.java
+++ expression-parser/Variable.java
@@ -35,7 +35,6 @@ public class Variable implements AllInterfaceExpression {
         }
     }
 
-    // :NOTE: не эффективно
     @Override
     public int evaluate(List<Integer> list) {
         return list.get(variableInt);
