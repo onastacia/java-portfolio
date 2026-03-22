c88a3cf8939a8419d3f6952af5e15c60d7efdbc4
onastacia <rebbrevsd@gmail.com>
2026-03-22 21:08:49 +0300

    Add files via upload

--- expression-parser/exceptions/ParsingException.java
+++ expression-parser/exceptions/ParsingException.java
@@ -1,6 +1,5 @@
 package expression.exceptions;
 
-// :NOTE: нужно проверяемое исключение
 public class ParsingException extends RuntimeException {
     public ParsingException(String message) {
         super(message);
